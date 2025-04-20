b922fb5e316e3a0422cd97590c9cab228015fd90
artem-totality <artem.totality@gmail.com>
2025-04-20 22:49:06 +0100

    Adjust carousel parameters

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log('Go');
+
+const myCarouselElement = document.querySelector('#carouselSlides');
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+	interval: 3000,
+	touch: true,
+});
