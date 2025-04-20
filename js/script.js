551cdbc79f99e44a011015daf038c8763ee07b38
artem-totality <artem.totality@gmail.com>
2025-04-20 23:13:14 +0100

    Fix bug with promo header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 console.log('Go');
 
-const myCarouselElement = document.querySelector('#carouselSlides');
+// const myCarouselElement = document.querySelector('#carouselSlides');
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-	interval: 3000,
-	touch: true,
-});
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+// 	interval: 3000,
+// 	touch: true,
+// });
