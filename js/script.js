3ee2425bea5106958ef48235379099a0b855dcfa
artem-totality <artem.totality@gmail.com>
2025-04-21 21:08:51 +0100

    Move static HTML to folder static

--- js/script.js
+++
@@ -1,8 +0,0 @@
-console.log('Go');
-
-// const myCarouselElement = document.querySelector('#carouselSlides');
-
-// const carousel = new bootstrap.Carousel(myCarouselElement, {
-// 	interval: 3000,
-// 	touch: true,
-// });
